b2550d8c101c743b449e79fcaea03e8cb95850d5
Sean-mn <b01080080329@gmail.com>
2025-05-12 14:31:54 +0900

    수정

--- Server/Server_Practice_Server/Server/Controllers/AuthController.cs
+++ Server/Server_Practice_Server/Server/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Server.interfaces;
+using Server.Models;
 
 namespace Server.Controllers
 {
@@ -9,5 +15,46 @@ namespace Server.Controllers
     {
         private readonly IConfiguration _configuration;
         private readonly IUserService _userService;
+
+        public AuthController(IConfiguration configuration, IUserService userService)
+        {
+            _configuration = configuration;
+            _userService = userService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest req)
+        {
+            var user = _userService.Authenticate(req.Username, req.Password);
+            if (user == null)
+                return Unauthorized("잘못된 요청");
+            
+            var token = GenerateJwtToken(user);
+        }
+        
+        private string GenerateJwtToken(User user)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),  // 1시간 동안 유효한 토큰
+                signingCredentials: creds
+            );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
